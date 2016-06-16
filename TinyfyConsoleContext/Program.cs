a4a2dfaabb6574d254f79dd5df57348fb5f197af
J4C0814N <J4C0814n@gmail.com>
2016-06-16 12:39:33 +1000

    Added basic concept idea so I don't forget :)
    when I eventually get around to developing this

--- TinyfyConsoleContext/Program.cs
+++ TinyfyConsoleContext/Program.cs
@@ -8,6 +8,11 @@ namespace TinyfyConsoleContext
 {
     class Program
     {
+        // See https://stackoverflow.com/questions/6179562/adding-my-program-to-right-click-menu
+        // Basic idea is to create a simple console app that takes an image argument and uses the 
+        // TinyPNG API https://tinypng.com/developers to compress and replace (or append name) the passed image
+        // With the addition of a windows context menu so a user can simply right click > compress a file
+ 
         static void Main(string[] args)
         {
         }
